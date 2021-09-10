9a567d54cbb9b8a3019d861b36dcea3e8d36c7e4
Samuel Heluany <samuelheluany@hotmail.com>
2021-09-10 11:31:45 -0300

    Done project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [lista, setLista] = useState([])
+  const [novoItem, setNovoItem] = useState("")
+
+  function adicionarNovoItem() {
+    setLista([...lista, novoItem])
+    setNovoItem("")
+  }
+
+  function deletarItem(index) {
+    const tmpArray = [...lista]
+    tmpArray.splice(index, 1)
+    setLista(tmpArray)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="todoList">
+        <input autoFocus
+          maxlength="30"
+          type="text"
+          value={novoItem}
+          onChange={e => setNovoItem(e.target.value)}
+          placeholder="Digite sua tarefa aqui!" />
+        <button className="buttonAdd" onClick={() => adicionarNovoItem()}>
+          <i class="fas fa-plus-circle fa-2x"></i>
+        </button>
+        <div className="list">
+          <ul>
+            {lista.map((item, index) =>
+              <li>
+                {item}
+                <div className="buttonDel">
+                  <button className="buttonDelete" onClick={() => deletarItem(index)}>
+                    <i class="fas fa-trash"></i>
+                  </button>
+                </div>
+              </li>)}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
